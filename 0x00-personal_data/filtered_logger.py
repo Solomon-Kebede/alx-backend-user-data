2b4d003cc386c8e5e34ef83b579a92b2b32fcff0
Solomon Kebede <solomonaau188@gmail.com>
2023-02-04 20:23:39 +0000

    Task 0 - doc fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,10 +29,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        '''Initialize redaction formatter'''
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        '''Return redacted result'''
         # print(record)
         # print()
         # print(self.REDACTION)
