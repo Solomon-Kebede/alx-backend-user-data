9c7c06f55ac36b3756878bff5644f83fa9176328
Solomon Kebede <solomonaau188@gmail.com>
2023-02-04 20:01:32 +0000

    Task 1 - Redacting formatter & field attribute

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,7 +17,23 @@ Write a function called `filter_datum` that returns the log message obfuscated:
 
 from typing import List
 import re
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 def filter_datum(
         fields: List[str],
