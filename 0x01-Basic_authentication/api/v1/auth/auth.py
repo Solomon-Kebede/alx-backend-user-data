18b82023e3811525703e69df620e54aef92ffcf7
Solomon Kebede <solomonaau188@gmail.com>
2023-02-07 17:03:14 +0000

    Task 3. Auth class: Create base class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module for Authentication
+"""
+from flask import request
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """docstrings for require_auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """docstrings for authorization_header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """docstrings for current_user"""
+        return None
