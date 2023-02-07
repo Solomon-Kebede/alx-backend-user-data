86858097ed44272cfab15ecbee9cfb1a071302e1
Solomon Kebede <solomonaau188@gmail.com>
2023-02-07 17:31:36 +0000

    Task 3. Auth class: Import & pycode fix

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,8 @@
 Module for Authentication
 """
 from flask import request
+from typing import List, TypeVar
+
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
