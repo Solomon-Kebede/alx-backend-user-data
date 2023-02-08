7ca4e8b5ed754ea49579e95c95eebfdceac7d219
Solomon Kebede <solomonaau188@gmail.com>
2023-02-08 01:31:55 +0000

    Task 6. Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -16,7 +17,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None or os.getenv("AUTH_TYPE")
 
 if auth:
-    auth = Auth()
+    if auth == "basic_auth":
+        auth = BasicAuth()
+    else:
+        auth = Auth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for Basic Authentication
+"""
+# from os import getenv
+# from api.v1.views import app_views
+# from flask import Flask, jsonify, abort, request
+# from flask_cors import (CORS, cross_origin)
+# import os
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """docstring for BasicAuth"""
+    pass
