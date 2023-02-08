49071e4e216e2603a4ecb5b5085f4c957704a948
Solomon Kebede <solomonaau188@gmail.com>
2023-02-08 02:44:12 +0000

    Task 10 - Add method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Module for Basic Authentication
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -52,3 +54,16 @@ class BasicAuth(Auth):
             return None, None
         else:
             return tuple(decoded_base64_authorization_header.split(':'))
+
+    def user_object_from_credentials(
+        self,
+        user_email: str,
+        user_pwd: str
+    ) -> TypeVar('User'):
+        """Returns the User instance based on his email and password"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        elif user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        else:
+            return (User.search({user_email: user_pwd}))
