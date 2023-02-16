4e09cf6252bfed493f6fed0dec0d8be1a98f0645
Solomon Kebede <solomonaau188@gmail.com>
2023-02-16 21:34:58 +0000

    Task  2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base
 from user import User
 
@@ -39,3 +40,15 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs):
+        # print(kwargs)
+        # print(User.__dict__)
+        # for k, v in kwargs.items():
+        try:
+            a = self._session.query(User).filter_by(**kwargs).one()
+            return (a)
+        except NoResultFound:
+            raise NoResultFound
+        except InvalidRequestError:
+            raise InvalidRequestError
